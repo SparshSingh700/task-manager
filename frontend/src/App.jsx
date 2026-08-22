38f396e6a839a4b7bba579fc2fd8a8e4fe6ab5f1
Sparsh Singh <sparshsingh700721@gmail.com>
2026-08-22 23:49:16 +0530

    Display tasks in React frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/tasks")
       .then((response)=> response.json())
-      .then((data)=> console.log(data))
+      .then((data)=> setTasks(data))
   }, []);
 
-  return (
-    <h1>Task Manager</h1>
-  )
+  return(
+    <div>
+      <h1>Task Manager</h1>
+
+      {
+        tasks.map((task) => (
+        <p key={task._id}>{task.title}</p>
+        ))
+      }
+
+    </div>
+  );
+
 }
 
 export default App
